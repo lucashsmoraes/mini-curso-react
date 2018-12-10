fc55ad8ed9e069ed66f0b44a87a6184b081f035a
Lucas Moraes <lucashsmoraes@gmail.com>
2018-12-10 20:07:36 -0200

    conectando o componente VideoList a Store

--- src/componentes/VideoList.js
+++ src/componentes/VideoList.js
@@ -1,52 +1,29 @@
 import React from 'react'
-
-import {List, Image} from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { List, Image } from 'semantic-ui-react'
 
 const VideoList = props => {
     return (
-        <div className= 'video-list'>
+        <div className='video-list'>
             <List animated verticalAlign='middle'>
                 <List.Item>
-                    <Image avatar src='https://bit.ly/2RQu1Sv'></Image>                
+                    <Image avatar src='https://bit.ly/2RQu1Sv'></Image>
                     <List.Content>
                         <List.Header>Titulo do video</List.Header>
                     </List.Content>
                 </List.Item>
             </List>
-            <List animated verticalAlign='middle'>
-                <List.Item>
-                    <Image avatar src='https://bit.ly/2RQu1Sv'></Image>                
-                    <List.Content>
-                        <List.Header>Titulo do video</List.Header>
-                    </List.Content>
-                </List.Item>
-            </List>
-            <List animated verticalAlign='middle'>
-                <List.Item>
-                    <Image avatar src='https://bit.ly/2RQu1Sv'></Image>                
-                    <List.Content>
-                        <List.Header>Titulo do video</List.Header>
-                    </List.Content>
-                </List.Item>
-            </List>
-            <List animated verticalAlign='middle'>
-                <List.Item>
-                    <Image avatar src='https://bit.ly/2RQu1Sv'></Image>                
-                    <List.Content>
-                        <List.Header>Titulo do video</List.Header>
-                    </List.Content>
-                </List.Item>
-            </List>
-            <List animated verticalAlign='middle'>
-                <List.Item>
-                    <Image avatar src='https://bit.ly/2RQu1Sv'></Image>                
-                    <List.Content>
-                        <List.Header>Titulo do video</List.Header>
-                    </List.Content>
-                </List.Item>
-            </List>
+            <p>{JSON.stringify(props)}</p>
         </div>
     )
 }
 
-export default VideoList
+const mapStateToProps = (state) => {
+    return {
+        videos: state.busca.videos,
+        carregando: state.busca.carregando,
+        erro: state.busca.erro
+    }
+}
+
+export default connect(mapStateToProps, null)(VideoList)
