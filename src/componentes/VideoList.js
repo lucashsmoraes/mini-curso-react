2f8b9f2e54ac986667458368155d2053e5ed59f8
Lucas Moraes <lucashsmoraes@gmail.com>
2018-12-10 18:33:15 -0200

    criando componente Video list

--- src/componentes/VideoList.js
+++ src/componentes/VideoList.js
@@ -1,9 +1,50 @@
 import React from 'react'
 
+import {List, Image} from 'semantic-ui-react'
+
 const VideoList = props => {
     return (
         <div className= 'video-list'>
-            <h1>Video List</h1>
+            <List animated verticalAlign='middle'>
+                <List.Item>
+                    <Image avatar src='https://bit.ly/2RQu1Sv'></Image>                
+                    <List.Content>
+                        <List.Header>Titulo do video</List.Header>
+                    </List.Content>
+                </List.Item>
+            </List>
+            <List animated verticalAlign='middle'>
+                <List.Item>
+                    <Image avatar src='https://bit.ly/2RQu1Sv'></Image>                
+                    <List.Content>
+                        <List.Header>Titulo do video</List.Header>
+                    </List.Content>
+                </List.Item>
+            </List>
+            <List animated verticalAlign='middle'>
+                <List.Item>
+                    <Image avatar src='https://bit.ly/2RQu1Sv'></Image>                
+                    <List.Content>
+                        <List.Header>Titulo do video</List.Header>
+                    </List.Content>
+                </List.Item>
+            </List>
+            <List animated verticalAlign='middle'>
+                <List.Item>
+                    <Image avatar src='https://bit.ly/2RQu1Sv'></Image>                
+                    <List.Content>
+                        <List.Header>Titulo do video</List.Header>
+                    </List.Content>
+                </List.Item>
+            </List>
+            <List animated verticalAlign='middle'>
+                <List.Item>
+                    <Image avatar src='https://bit.ly/2RQu1Sv'></Image>                
+                    <List.Content>
+                        <List.Header>Titulo do video</List.Header>
+                    </List.Content>
+                </List.Item>
+            </List>
         </div>
     )
 }
