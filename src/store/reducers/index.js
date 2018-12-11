aebad3a55848267a7b397cef30678ed0c940c7da
Lucas Moraes <lucashsmoraes@gmail.com>
2018-12-11 00:38:08 -0200

    redux sincrono: reproduz video

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import busca from './busca'
+import reproduzVideo from './reproduz-video'
 
 const rootReducer = combineReducers({
-    busca
+    busca,
+    reproduzVideo
 })
 
 
