6138376ba7794baaa31a3f5bbb6a894867a6edfd
Lucas Moraes <lucashsmoraes@gmail.com>
2018-12-11 00:30:11 -0200

    spinner enquanto carrega video

--- src/componentes/VideoList.js
+++ src/componentes/VideoList.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { List, Image } from 'semantic-ui-react'
+import { List, Image, Dimmer, Loader } from 'semantic-ui-react'
 
 class VideoList extends Component {
 
     renderVideo(video) {
         return (
-            <List animated verticalAlign='middle'>
+            <List animated verticalAlign='middle' key={video.etag}>
                 <List.Item>
                     <Image src={video.snippet.thumbnails.default.url}></Image>
                     <List.Content>
@@ -20,6 +20,12 @@ class VideoList extends Component {
     render() {
         return (
             <div className='video-list'>
+            {
+                this.props.carregando && (
+                <Dimmer active inverted>
+                    <Loader size="medium">Carregando...</Loader>
+                </Dimmer>)
+            }
                {
                    this.props.videos.map(video => {
                        console.log("meu video", video)
